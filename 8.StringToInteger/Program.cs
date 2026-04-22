bc4295d741e0db71a108cc2e9f5bcc8d82e859ef
NguyenLoc2005 <nxl522005@gmail.com>
2026-04-22 16:37:23 +0700

    StringToIntege

---
+++ 8.StringToInteger/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+class StringToInteger
+{
+    static void Main()
+    {
+        string str = " 42";
+        for(int i=0; i < str.Length; i++)
+        {
+            if(str[i]== ' ')
+            {
+                continue;
+            }
+
+            int accessible = (int)(str[i] - '0'); 
+            Console.Write(accessible);
+        }
+    }
+}
