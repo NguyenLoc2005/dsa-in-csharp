a93007974115649c97dea1779a1d0d99006f14c4
NguyenLoc2005 <nxl522005@gmail.com>
2026-04-22 19:39:11 +0700

    PalinromeNumber

---
+++ 9.PalindromeNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿
+class PalinromeNumber
+{
+    static void Main()
+    {
+        int x = -121;
+        string str = x.ToString();
+        bool isPalindrome = true;
+        if (x < 0)
+        {
+            isPalindrome = false;
+        }
+
+        int left =0;
+        int right = str.Length - 1;
+        while(left < right)
+        {
+            left ++;
+            right --;
+            if(str[left] != str[right])
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome)
+        {
+            Console.WriteLine("The number is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine("The number is not a palindrome.");
+        }
+    }
+}
+
+
