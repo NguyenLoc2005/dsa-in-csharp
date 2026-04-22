a5e3a13a8aa62d5fe522d2962d8324f0170de32e
NguyenLoc2005 <nxl522005@gmail.com>
2026-04-22 16:22:49 +0700

    ReverseInteger

--- 7.ReverseInteger/Program.cs
+++ 7.ReverseInteger/Program.cs
@@ -2,7 +2,7 @@
 
 public class ReverseInteger {
     static void Main(string[] args) {
-        int x = -12220;
+        int x = -302220;
         
         // In dấu trừ trước nếu là số âm
         if (x < 0) {
@@ -14,9 +14,10 @@ public class ReverseInteger {
         while (x > 0) {
             int du = x % 10;          // Lấy chữ số cuối
             result = result * 10 + du; // Đẩy chữ số cũ sang trái và cộng số mới vào
-            x = x / 10;                // Rút gọn x
+            x = x / 10;                // Rút gọn x .
         }
 
         Console.WriteLine(result); 
+
     }
 }
--- 7.ReverseInteger/obj/Debug/net10.0/ReverseInteger.AssemblyInfo.cs
+++ 7.ReverseInteger/obj/Debug/net10.0/ReverseInteger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReverseInteger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36360989c45c7a3d6f7e1f37e417fb8b804ee9e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31b6f22d2988677fe837786e23a67974406f08b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReverseInteger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReverseInteger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
